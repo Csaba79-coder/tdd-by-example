7a999e65c5639056053a028085c3ff7c21cc616f
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2021-11-19 15:14:38 +0100

    Summary
    refactor code + test (making Money class to be responsilbe for most of the things!)

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -2,11 +2,14 @@ package guru.springframework;
 
 public class Dollar extends Money {
 
-    public Dollar(int amount) {
-        this.amount = amount;
+    // private String currency;
+
+    public Dollar(int amount, String currency) {
+        super(amount, currency);
     }
 
     public Money times(int multiplier) {
-        return new Dollar(amount * multiplier);
+        // return new Dollar(amount * multiplier);
+        return Money.dollar(amount * multiplier);
     }
 }
--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -54,5 +54,11 @@ public class MoneyTest {
         //assertNotEquals(new Franc(5), new Franc(8));
         assertNotEquals(Money.franc(5), Money.franc(8));
     }
+
+    @Test
+    void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
 }
 
