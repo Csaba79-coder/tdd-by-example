9348759b6718e80acd1eaae0a388495005d636f4
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2021-11-19 20:53:36 +0100

    Summary
    STOP duplication of Dollar and Franc, make refactor!

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -8,8 +8,11 @@ public class Dollar extends Money {
         super(amount, currency);
     }
 
-    public Money times(int multiplier) {
-        // return new Dollar(amount * multiplier);
-        return Money.dollar(amount * multiplier);
-    }
+//    public Money times(int multiplier) {
+//        // return new Dollar(amount * multiplier);
+//        //return Money.dollar(amount * multiplier);,
+//        // return new Dollar(amount * multiplier, "USD");
+//        // return new Dollar(amount * multiplier, this.currency);
+//        return new Money(amount * multiplier, this.currency);
+//    }
 }
